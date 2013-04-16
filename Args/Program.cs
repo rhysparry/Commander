d645af85d123907dd0b7b654104bb9f0855c42f0
Rhys Parry <rhys@i-think22.net>
2013-04-16 20:44:30 +1000

    Args utility
    Args shows the command line arguments one per line. Useful for working
    out how arguments are separated.

---
+++ Args/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Args
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.Out.WriteLine(arg);
+            }
+        }
+    }
+}
